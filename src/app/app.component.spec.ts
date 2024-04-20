5033018996666007a36f50650953e93a25173be5
zurachan <manchestervn199@gmail.com>
2024-04-20 14:34:01 +0700

    update unitest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular17demo');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello world from angular17');
   });
 });
