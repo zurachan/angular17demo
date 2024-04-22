34d8c639402c046172f1f285eeeabf29e684a143
zurachan <manchestervn199@gmail.com>
2024-04-22 12:01:47 +0700

    dựng Portfolio

---
+++ src/app/layout/aside/aside.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aside',
+  standalone: true,
+  templateUrl: './aside.component.html',
+  styleUrls: ['./aside.component.css'],
+})
+export class AsideComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/layout/content/content.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css'],
+})
+export class ContentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
